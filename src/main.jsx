dd6f06a21afe7840262dc5112e7265031e0eb3b9
YURI BRUNO HAYASHIDA <129120872+yurihay@users.noreply.github.com>
2026-06-25 12:04:34 -0300

    feat: use enhanced app with message controls

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './AppPlus.jsx';
 import './styles.css';
 
 const el = globalThis.document?.querySelector('#root');
