bbe9377eb229a8da1ad3a2b3da841c8ec99b476f
YURI BRUNO HAYASHIDA <129120872+yurihay@users.noreply.github.com>
2026-06-25 12:05:39 -0300

    style: load message controls css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './AppPlus.jsx';
 import './styles.css';
+import './messageControls.css';
 
 const el = globalThis.document?.querySelector('#root');
 createRoot(el).render(React.createElement(App));
